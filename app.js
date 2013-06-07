309b1b196b9f69ae573451588d1bd0f7cd40d713
Michal Kowalkowski <malcolm.anteros@gmail.com>
2013-06-07 22:17:42 +0800

    favicon

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(express.favicon(__dirname + '/public/favicon.ico')); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
